1c0cdcaabc48546a315d850a82efd95847e9487c
muddasarnazaransari <sprituate@gmail.com>
2025-06-27 03:08:31 +0530

    images not showing

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()],
-    base: '/todoApp/'
+  plugins: [react()],
+  base: '/todoApp/' // 👈 Must match your GitHub repo name exactly (case-sensitive)
 })
